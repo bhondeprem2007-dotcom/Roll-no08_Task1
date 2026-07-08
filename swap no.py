f1fcfe5760ddf75f2c3ac9b97d4e44f056e5bdd0
Ansh Gajanan Bhonde <bhonde.prem2007@gmail.com>
2026-07-09 00:41:05 +0500

    Add files via upload

---
+++ swap no.py
@@ -0,0 +1,10 @@
+## Write a program to swap two numbers.
+
+num1 = int(input("Enter the first value : "))
+num2 = int(input("Enter the second value : "))
+
+num1, num2 = num2, num1
+
+print( "Number is swaped")
+
+print("First No. is", num1,"\nSecond No.is ", num2)
