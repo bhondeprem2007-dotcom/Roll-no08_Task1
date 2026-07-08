2a07129e1d39967cc2674869501d4a82f004c666
Ansh Gajanan Bhonde <bhonde.prem2007@gmail.com>
2026-07-09 00:46:21 +0500

    TASK -1 Basic Programing

---
+++ Basic pytthon/additon.py
@@ -0,0 +1,7 @@
+ ## Write a program to add two numbers entered by the user.
+
+num1 = int(input("Enter the first value : "))
+num2 = int(input("Enter the second value : "))
+
+print("Sum of two no.:", num1 + num2)
+
