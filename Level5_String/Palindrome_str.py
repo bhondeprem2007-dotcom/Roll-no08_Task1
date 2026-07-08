9c2413c47f203e7b9070905568beb8f20c65f556
Ansh Gajanan Bhonde <bhonde.prem2007@gmail.com>
2026-07-09 02:43:08 +0500

    Add files via upload

---
+++ Level5_String/Palindrome_str.py
@@ -0,0 +1,10 @@
+##Write a program to check whether a string is a palindrome.
+
+
+text = input("Enter a string: ")
+
+
+if text == text[::-1]:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
