8a713642df02555034a2752380c59ccbea8cac40
Ansh Gajanan Bhonde <bhonde.prem2007@gmail.com>
2026-07-09 18:16:01 +0500

    Add files via upload

---
+++ Level8_Tuple and Dictionaries/Tuple and Dictionary.py
@@ -0,0 +1,15 @@
+## Write a program to create and display a dictionary.
+
+try:
+     student = {
+         "Name": input("Enter name : "),
+         "Age": int(input("Enter the Age :")),
+         "Course": input("Enter the Course : ")
+}
+except "Age" << 100 :
+     ValueError
+    
+finally :
+
+  print("Student Dictionary:")
+  print(student)
